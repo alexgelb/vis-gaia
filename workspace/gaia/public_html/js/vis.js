da673df81dc524ea327b7fb03ddd9b48e091b642
Nikollos <nicole.cherches@hotmail.com>
2017-12-12 22:26:01 +0100

    deleted empty columns in dataset

--- workspace/gaia/public_html/js/vis.js
+++ workspace/gaia/public_html/js/vis.js
@@ -545,10 +545,10 @@ function correlation(MultipleData) {
 
     var filtered_data = csv_data.filter(function(d) {
         return MultipleData.reduce(function(acc, column) {
-            return acc && +d[column]!=null && +d[column] != -999 && !isNaN(+d[column]);
+            return acc && +d[column] !== null && +d[column] != -999 && !isNaN(+d[column]);
         }, true);
     });
-    console.log(filtered_data);
+
     for (var i = 0; i < MultipleData.length; i++) {
         for (var j = 0; j < MultipleData.length; j++) {
             var col1 = filtered_data.map(function(d) {
